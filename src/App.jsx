0c57fc219e726ef361d89a7148b4a808baf0ac8e
Francisco Estivallet <francisco@odd.group>
2025-03-03 16:42:30 +0100

    fix zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MapView from './components/MapView' // Import the MapView component
+import MapViewHeatmap from './components/MapView-heatmap' // Import the MapView-heatmap component
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,9 @@ function App() {
   return (
     <>
       <div style={{ height: '100vh', width: '100vw' }}>
-        <MapView /> 
+        {/* <MapView />  */}
+        <MapViewHeatmap />
+
       </div>
     </>
   )

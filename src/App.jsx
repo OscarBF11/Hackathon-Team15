5e567e357df24e6410a5cef82df867b01be15cbd
Francisco Estivallet <francisco@odd.group>
2025-03-03 15:25:54 +0100

    base for heatmap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MapView from './components/MapView' // Import the MapView component
+import MapViewHeatmap from './components/MapView-heatmap' // Import the MapViewHeatmap component
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div style={{ height: '100vh', width: '100vw' }}>
-        <MapView /> 
+        {/* <MapView />  */}
+        <MapViewHeatmap />
       </div>
     </>
   )
